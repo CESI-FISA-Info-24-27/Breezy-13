d8d1720467c1c3067f6faf235f278961603fbe4b
Sacha <sacha.lisbona@gmail.com>
2025-06-02 14:15:32 +0200

    feat : developpement des services de rôle et d'utilisateur #5

--- API/Services/UsersServices.js
+++ API/Services/UsersServices.js
@@ -1,1 +1,88 @@
-// TODO
+import { DAOMongoDbFactory } from "../Factory/DAOMongoDbFactory.js";
+
+const Factory = new DAOMongoDbFactory();
+const UsersDAO = Factory.createUsersDAO();
+
+/**
+ * Represents a service for handling users requests
+ */
+const UsersServices = {
+    /**
+     * Get users
+     * @param {object} filters - The filters to apply
+     * @returns {Array} - An array of users
+     */
+    getUsers: async (filters) => {
+        return await UsersDAO.getUsers(filters);
+    },
+
+    // /**
+    //  * Update users
+    //  * @param {number} userId - The id of the user to update
+    //  * @param {object} user - The user data to update
+    //  * @returns {object} - The user updated
+    //  */
+    // updateUsers: async (userId, user) => {
+    //     return await UsersDAO.updateUser(userId, user);
+    // },
+
+    // /**
+    //  * Delete users
+    //  * @param {number} userId - The id of the user to delete
+    //  * @returns {object} - The user deleted
+    //  */
+    // deleteUsers: async (userId) => {
+    //     return await UsersDAO.deleteUser(userId);
+    // },
+
+    /**
+     * Create users
+     * @param {object} user - The user to create
+     * @returns {object} - The user created
+     */
+    createUsers: async (user) => {
+        return await UsersDAO.createUser(user);
+    },
+
+    /**
+     * Add a revoked token in database
+     * @param {string} token - The token to add
+     * @returns {object} - The token added
+     * @async
+     */
+    addrevokedtoken: async (token) => {
+        return await UsersDAO.addrevokedtoken(token);
+    },
+
+    /**
+     * Is the token revoked
+     * @param {string} token - The token to check
+     * @returns {boolean} - True if the token is revoked, false otherwise
+     * @async
+     */
+    isTokenRevoked: async (token) => {
+        return await UsersDAO.isTokenRevoked(token);
+    },
+
+    /**
+     * Get the role of a user
+     * @param {number} userId - The id of the user
+     * @returns {object} - The role of the user
+     * @async
+     */
+    getRole: async (userId) => {
+        return await UsersDAO.getRole(userId);
+    },
+
+    // /**
+    //  * Get the permissions of a user
+    //  * @param {number} userId - The id of the user
+    //  * @returns {object} - The permissions of the user
+    //  * @async
+    //  */
+    // getPermissions: async (userId) => {
+    //     return await UsersDAO.getPermissions(userId);
+    // }
+}
+
+export default UsersServices;
