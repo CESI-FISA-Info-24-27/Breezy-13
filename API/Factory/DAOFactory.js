16d8ec748eb4c70ca66860ed763df286f74fd579
SERRE Loïc <loic.serre@viacesi.fr>
2025-06-02 12:30:43 +0200

    Création des Factory pour l'API REST #6

--- API/Factory/DAOFactory.js
+++ API/Factory/DAOFactory.js
@@ -1,1 +1,47 @@
-// TODO
+/**
+ * Représente une Factory pour créer des objets DAOs
+ * @class DAOFactory
+ */
+export class DAOFactory {
+    constructor() {}
+
+    /**
+     * Crée un objet DAOFactory pour les rôles
+     * @returns {RolesDAO} - Le DAO des rôles
+     */
+    createRolesDAO() {
+        throw new Error('Non implémenté');
+    }
+
+    /**
+     * Crée un objet DAOFactory pour les utilisateurs
+     * @returns {UsersDAO} - Le DAO des utilisateurs
+     */
+    createUsersDAO() {
+        throw new Error('Non implémenté');
+    }
+
+    /**
+     * Crée un objet DAOFactory pour les posts
+     * @returns {PostsDAO} - Le DAO des posts
+     */
+    createPostsDAO() {
+        throw new Error('Non implémenté');
+    }
+
+    /**
+     * Crée un objet DAOFactory pour les commentaires
+     * @returns {CommentsDAO} - Le DAO des commentaires
+     */
+    createCommentsDAO() {
+        throw new Error('Non implémenté');
+    }
+
+    /**
+     * Crée un objet DAOFactory pour les relations de suivi
+     * @returns {FollowsDAO} - Le DAO des relations de suivi
+     */
+    createFollowsDAO() {
+        throw new Error('Non implémenté');
+    }
+}
---
+++ API/Factory/DAOMongoDbFactory.js
@@ -0,0 +1,61 @@
+import { DAOFactory } from "./DAOFactory.js";
+import { UsersMongoDAO } from "../DAO/UsersMongoDAO.js";
+import { RolesMongoDAO } from "../DAO/RolesMongoDAO.js";
+import { PostsMongoDAO } from "../DAO/PostsMongoDAO.js";
+import { CommentsMongoDAO } from "../DAO/CommentsMongoDAO.js";
+import { FollowsMongoDAO } from "../DAO/FollowsMongoDAO.js";
+
+/**
+ * Représente une Factory pour créer des objets DAO spécifiques à MongoDB
+ * @extends DAOFactory
+ * @class DAOMongoDbFactory
+ */
+export class DAOMongoDbFactory extends DAOFactory {
+    /**
+     * Crée une nouvelle instance de DAOMongoDbFactory
+     * @constructor
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     * Crée une nouvelle instance de UsersMongoDAO
+     * @returns {UsersMongoDAO} - Une nouvelle instance de UsersMongoDAO
+     */
+    createUsersDAO() {
+        return new UsersMongoDAO();
+    }
+
+    /**
+     * Crée une nouvelle instance de RolesMongoDAO
+     * @returns {RolesMongoDAO} - Une nouvelle instance de RolesMongoDAO
+     */
+    createRolesDAO() {
+        return new RolesMongoDAO();
+    }
+
+    /**
+     * Crée une nouvelle instance de PostsMongoDAO
+     * @returns {PostsMongoDAO} - Une nouvelle instance de PostsMongoDAO
+     */
+    createPostsDAO() {
+        return new PostsMongoDAO();
+    }
+
+    /**
+     * Crée une nouvelle instance de CommentsMongoDAO
+     * @returns {CommentsMongoDAO} - Une nouvelle instance de CommentsMongoDAO
+     */
+    createCommentsDAO() {
+        return new CommentsMongoDAO();
+    }
+
+    /**
+     * Crée une nouvelle instance de FollowsMongoDAO
+     * @returns {FollowsMongoDAO} - Une nouvelle instance de FollowsMongoDAO
+     */
+    createFollowsDAO() {
+        return new FollowsMongoDAO();
+    }
+}
