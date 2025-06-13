8f8223d98c043225a70656a113a1cbbe4e87dd85
SERRE Loïc <loic.serre@viacesi.fr>
2025-06-13 16:28:26 +0200

    Ajout des services pour les requetes http à l'API

---
+++ webapp/src/services/usersServices.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import { refreshToken } from "./authService";
+
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/users`;
+
+function getAuthHeader() {
+  const token = Cookies.get("token") || localStorage.getItem("token");
+  return token ? { Authorization: `${token}` } : {};
+}
+
+async function withAuthRetry(requestFn) {
+  try {
+    return await requestFn(getAuthHeader());
+  } catch (err) {
+    if (err.response && err.response.status === 401) {
+      const newToken = await refreshToken();
+      return await requestFn({ Authorization: `${newToken}` });
+    }
+    throw err;
+  }
+}
+
+export async function getUsers() {
+  return withAuthRetry(headers =>
+    axios.get(API_URL, { headers }).then(res => res.data)
+  );
+}
+
+export async function createUser(user) {
+  return withAuthRetry(headers =>
+    axios.post(API_URL, user, { headers }).then(res => res.data)
+  );
+}
+
+export async function updateUser(id, user) {
+  return withAuthRetry(headers =>
+    axios.patch(`${API_URL}/${id}`, user, { headers }).then(res => res.data)
+  );
+}
+
+export async function deleteUser(id) {
+  return withAuthRetry(headers =>
+    axios.delete(`${API_URL}/${id}`, { headers }).then(res => res.data)
+  );
+}
