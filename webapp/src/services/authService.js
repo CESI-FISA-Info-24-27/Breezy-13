39065924d8f922d89205e9346dbb6818dbb627a0
SERRE Loïc <loic.serre@viacesi.fr>
2025-06-13 16:28:01 +0200

    Ajout refresh token pour les requêtes

--- webapp/src/services/authService.js
+++ webapp/src/services/authService.js
@@ -1,15 +1,30 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const REFRESH_URL = process.env.NEXT_PUBLIC_REFRESH_URL;
+const LOGIN_URL = process.env.NEXT_PUBLIC_LOGIN_URL;
 
 export async function login(email, password, rememberMe = false) {
     try {
-        const response = await axios.post('/api/auth', {
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth`, {
             email,
             password,
             rememberMe
         });
         return response.data;
     } catch (error) {
-        // Gérer les erreurs (important pour la production)
         throw error.response?.data || { message: 'Une erreur est survenue.' };
+    }
+}
+
+export async function refreshToken() {
+    try {
+        const res = await axios.post(REFRESH_URL, {}, { withCredentials: true });
+        const newToken = res.data.token;
+        Cookies.set("token", newToken);
+        return newToken;
+    } catch {
+        window.location.href = LOGIN_URL;
+        throw new Error("Session expirée");
     }
 }
