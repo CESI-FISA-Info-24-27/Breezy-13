5162b566b8c778e912898729a676845130f7af22
Sacha <sacha.lisbona@gmail.com>
2025-06-19 09:40:48 +0200

    Push de stabilisation apres rebase

--- webapp/src/app/profil-page/page.js
+++ webapp/src/app/profil-page/page.js
@@ -64,7 +64,6 @@ export default function HomePage() {
 
   return (
     <div className="relative min-h-screen bg-seasalt">
-      {/* Header */}
       <div
         ref={headerRef}
         className="fixed top-0 left-0 w-full z-50 transition-all duration-300"
