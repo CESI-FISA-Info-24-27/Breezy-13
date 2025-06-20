19af17407648299231a73aef4ffd94dad87e3d65
Beroud-Dylan <dylan.beroud@viacesi.fr>
2025-06-20 15:01:56 +0200

    feat : tests unitaires de la web app lancés automatiquement à chaque push / pull #39 #44

--- webapp/src/app/register/page.js
+++ webapp/src/app/register/page.js
@@ -2,12 +2,16 @@
 
 import Link from "next/link";
 import Image from "next/image";
+<<<<<<< Updated upstream
+import { useState } from "react";
+=======
 import { useRef, useState } from "react";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
-import { createUser, updateUser, getUsers } from "../../services/usersServices";
-import { login } from "../../services/authServices";
-import { uploadFile } from "../../services/fileServerService";
+import { createUser, updateUser } from "../../services/UsersServices";
+import { login } from "../../services/AuthServices";
+>>>>>>> Stashed changes
 
 export default function Register() {
   const [form, setForm] = useState({
